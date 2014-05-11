ab8fc5b49cd5d7fee3ae5bf2f14f9bcfd91b9729
Mitch Pronschinske <mpronschinske@gmail.com>
2014-05-10 23:41:58 -0400

    added gems, devise setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rorwiki::Application.config.secret_key_base = 'd7402d6dbf174565bccda9e4d7a4a1cf1393ab095d6cfe8898c5744d5efee286989d38642253e378c70f9821bbeb027a296d49fde087940c53bc2a9e9b5ef6c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rorwiki::Application.config.session_store :cookie_store, key: '_rorwiki_session'
